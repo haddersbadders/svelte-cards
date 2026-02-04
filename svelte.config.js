c06c05c8c0ee6944277ab4ccbbac94c11b653dad
Hadrian Cawthorne <h.p.cawthorne@sheffield.ac.uk>
2026-02-04 15:09:15 +0000

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto'; // Change this back to auto
+import adapter from '@sveltejs/adapter-cloudflare'; // Change this back to auto
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
